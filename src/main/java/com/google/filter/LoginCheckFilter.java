1447bed0aeed2732bf19cfbd10c2960f163498a4
tejas <tejasshah@gmail.com>
2023-04-05 09:54:38 +0530

    filter and toastr

---
+++ src/main/java/com/google/filter/LoginCheckFilter.java
@@ -0,0 +1,39 @@
+package com.google.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoginCheckFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpSession session = ((HttpServletRequest) request).getSession();
+		System.out.println("LoginCheckFilter called.....");
+
+		String url = ((HttpServletRequest) request).getRequestURI();
+		System.out.println(url);
+		if (url.contains("/assets") || url.contains("/login") || url.contains("/logout") || url.contains("/signup") || url.contains("/forgetpassword") || url.contains("/authentication") || url.contains("/saveuser") || url.contains("/updatepasswordjspopen") || url.contains("/updatemypassword") || url.contains("/sendotpforforgetpassword") ) {
+			chain.doFilter(request, response);
+		} else {
+			if (session.getAttribute("user") == null) {
+				System.out.println("============");
+				request.setAttribute("error", "Please Login Before Access the services");
+				RequestDispatcher rd = request.getRequestDispatcher("login");
+				rd.forward(request, response);
+			}
+		}
+	}
+
+}
--- src/main/webapp/WEB-INF/views/AdminHeader.jsp
+++ src/main/webapp/WEB-INF/views/AdminHeader.jsp
@@ -1,3 +1,5 @@
+ 
+
 <header id="header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
